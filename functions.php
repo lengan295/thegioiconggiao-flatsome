475cf3144bfa35c4ce70b4bc2cafbf8f61f98c78
Le Ngan <le.ngan@lundimatin.fr>
2023-10-26 15:46:04 +0700

    thegioiconggiao.vn : product page (WIP)

--- functions.php
+++ functions.php
@@ -2,11 +2,23 @@
 // Add custom Theme Functions here
 // add customize javascript
 add_action('wp_enqueue_scripts', function () {
-   // IONIONCS
-  wp_enqueue_style('ionicons', get_stylesheet_directory_uri() . '/css/ionicons.css', array(), rand(111,9999), 'all');
-  wp_enqueue_style('header', get_stylesheet_directory_uri() . '/css/header.css', array(), rand(111,9999), 'all');
-  wp_enqueue_style('footer', get_stylesheet_directory_uri() . '/css/footer.css', array(), rand(111,9999), 'all');
-// wp_enqueue_script('owl-carousel', get_stylesheet_directory_uri() . '/assets/js/owl.carousel.min.js', array('jquery'), null, true);
+    // OWL CAROUSEL
+    wp_enqueue_style('owl-carousel', get_stylesheet_directory_uri() . '/css/owl.carousel.min.css', array(), null, 'all');
+    wp_enqueue_style('owl-carousel-default', get_stylesheet_directory_uri() . '/css/owl.theme.default.min.css', array(), null, 'all');
+    wp_enqueue_script('owl-carousel', get_stylesheet_directory_uri() . '/js/owl.carousel.min.js', array('jquery'), '2.3.4', true);
+    // IONIONCS
+    wp_enqueue_style('ionicons', get_stylesheet_directory_uri() . '/css/ionicons.css', array(), rand(111, 9999), 'all');
+    wp_enqueue_style('header', get_stylesheet_directory_uri() . '/css/header.css', array(), rand(111, 9999), 'all');
+    wp_enqueue_style('footer', get_stylesheet_directory_uri() . '/css/footer.css', array(), rand(111, 9999), 'all');
+
+    if (is_front_page()) {
+        wp_enqueue_style('front-page', get_stylesheet_directory_uri() . '/css/front-page.css', array(), rand(111, 9999), 'all');
+        wp_enqueue_script('front-page', get_stylesheet_directory_uri() . '/js/front-page.js', array('jquery'), rand(111, 9999), true);
+    }
+
+    if (is_product()) {
+        wp_enqueue_style('product-page', get_stylesheet_directory_uri() . '/css/product-page.css', array(), rand(111, 9999), 'all');
+    }
 });
 
 // Turn off auto gen <p> of contact form 7
--- shortcodes.php
+++ shortcodes.php
@@ -1,11 +1,53 @@
 <?php
-add_shortcode('ionicon', function($atts){
-	$attributes = shortcode_atts(
+add_shortcode('ionicon', function ($atts) {
+    $attributes = shortcode_atts(
         array(
-			'class' => ''
+            'class' => ''
         ),
         $atts
     );
-	extract($attributes);
-	return '<i class="'.$class.'"></i>';
+    extract($attributes);
+    return '<i class="' . $class . '"></i>';
+});
+
+// Single product description
+add_shortcode('product-full-description', function () {
+    global $product;
+    try {
+        if (is_a($product, 'WC_Product')) {
+            return wc_format_content($product->get_description("shortcode"));
+        }
+        return "Product description shortcode run outside of product context";
+    } catch (Exception $e) {
+        return "Product description shortcode encountered an exception";
+    }
+});
+
+// Single product attributes
+add_shortcode('product-attributes', function ($atts) {
+    global $product;
+
+    if (!is_object($product) || !$product->has_attributes()) {
+        return '';
+    }
+
+    $attributes = $product->get_attributes();
+    $html = '';
+
+    if (!empty($attributes)) {
+
+        /** @var WC_Product_Attribute $attribute */
+        foreach ($attributes as $attribute) {
+
+                $attribute_label = $attribute->get_name();
+                $attribute_value = implode(', ', $attribute->get_options());
+
+                $html .= '<li>' . esc_html($attribute_label) . ' : ' . esc_html($attribute_value) . '</li>';
+
+        }
+
+        $html = '<ul>' . $html . '</ul>';
+    }
+
+    return $html;
 });
